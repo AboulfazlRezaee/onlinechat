7585a3ef735163a7519ba27199e70a1e52684cfc
AboulfazlRezaee <rezaee.aboulfazl@gmail.com>
2024-01-08 10:17:35 +0330

    this is the online chat app with django and agora

---
+++ base/views.py
@@ -0,0 +1,68 @@
+from unicodedata import name
+from django.shortcuts import render
+from django.http import JsonResponse
+from agora_token_builder import RtcTokenBuilder
+import random
+import time
+import json
+from .models import RoomMember
+from django.views.decorators.csrf import csrf_exempt
+
+def getToken(request):
+    appId = '56b67218a1fe48ac96e78bd1f2eef961'
+    appCertificate = '5d902e859464432bab712140fb2096fa'
+    channelName = request.GET.get('channel')
+    uid = random.randint(1, 230)
+    expirationTimeInSeconds = 3600 * 24
+    currentTimeStamp = time.time()
+    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
+    role = 1
+
+    token = RtcTokenBuilder.buildTokenWithUid(appId, appCertificate, channelName, uid, role, privilegeExpiredTs)
+    return JsonResponse({'token': token, 'uid': uid}, safe=False)
+
+
+def lobby(request):
+    return render(request, 'base/lobby.html')
+
+def room(request):
+    return render(request, 'base/room.html')
+
+
+@csrf_exempt
+def createMember(request):
+    data = json.loads(request.body)
+
+    member, created = RoomMember.objects.get_or_create(
+        name=data['name'],
+        uid=data['UID'],
+        room_name=data['room_name']
+    )
+    return JsonResponse({'name':data['name']}, safe=False)
+
+
+def getMember(request):
+    uid = request.GET.get('UID')
+    room_name = request.GET.get('room_name')
+
+    member = RoomMember.objects.get(
+        uid=uid,
+        room_name=room_name,
+    )
+
+    name = member.name
+
+    return JsonResponse({'name':member.name}, safe=False)
+
+
+@csrf_exempt
+def deleteMember(request):
+    data = json.loads(request.body)
+
+    member = RoomMember.objects.get(
+        name=data['name'],
+        uid=data['UID'],
+        room_name=['room_name'],
+    )
+    member.delete()
+    return JsonResponse('Member was deleted', safe=False)
